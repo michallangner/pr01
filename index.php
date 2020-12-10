86e2c81791b05d97b6dcb757e989b195b3787d4f
Your Name <you@example.com>
2020-12-10 12:06:30 +0100

    couchdb

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 
+
+//We need to use an autoloader to import PHPOnCouch classes
+//I will use composer's autoloader for this demo
+$autoloader = join(DIRECTORY_SEPARATOR,[__DIR__,'vendor','Autoload.php']);
+require $autoloader;
+
+//We import the classes that we need
+use PHPOnCouch\CouchClient;
+use PHPOnCouch\Exceptions;
+
+//We create a client to access the database
+$client = new CouchClient('http://admin:manager1@146.59.17.22:5984','myownpocket');
+
+//We create the database if required
+if(!$client->databaseExists()){
+    $client->createDatabase();
+}
+
+//We get the database info just for the demo
+var_dump($client->getDatabaseInfos());
+
+//Note:  Every request should be inside a try catch since CouchExceptions could be thrown.For example, let's try to get a unexisting document
+
+try{
+    $client->getDoc('the id');
+    echo 'Document found';
+}
+catch(Exceptions\CouchNotFoundException $ex){
+    if($ex->getCode() == 404)
+        echo 'Document not found';
+}
+
 // Show all information, defaults to INFO_ALL
 phpinfo();
 
